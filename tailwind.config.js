7b4e62d06f2341d04be9094fdf82f7ab572d1cbb
Alice <alize_in_cryptoland@protonmail.com>
2021-11-23 00:06:37 +0100

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: {
+    // content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    // safelist: [
+    //   'loading'
+    // ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+  ],
+}
