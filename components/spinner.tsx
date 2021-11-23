f6db1b07c0df59f9d11cdb72fc0a205e4513eb5e
Michael Gerullis <40480041+21e8@users.noreply.github.com>
2021-11-23 16:28:14 +0100

    add spinner

---
+++ components/spinner.tsx
@@ -0,0 +1,34 @@
+export const Spinner = () => <svg
+xmlns="http://www.w3.org/2000/svg"
+xmlnsXlink="http://www.w3.org/1999/xlink"
+style={{
+  margin: "auto",
+  background:
+    "background: rgb(241, 242, 243) none repeat scroll 0% 0%",
+  display: "inline",
+}}
+width="24px"
+height="24px"
+viewBox="0 0 100 100"
+preserveAspectRatio="xMidYMid"
+>
+<circle
+  cx="50"
+  cy="50"
+  r="32"
+  strokeWidth="8"
+  stroke="#85a2b6"
+  strokeDasharray="50.26548245743669 50.26548245743669"
+  fill="none"
+  strokeLinecap="round"
+>
+  <animateTransform
+    attributeName="transform"
+    type="rotate"
+    repeatCount="indefinite"
+    dur="1s"
+    keyTimes="0;1"
+    values="0 50 50;360 50 50"
+  ></animateTransform>
+</circle>
+</svg>
